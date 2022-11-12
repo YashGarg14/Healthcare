dd41b227c2b00337d59736326b573b912dff1ec9
YashGarg14 <yash.raj.garg2003@gmail.com>
2022-11-13 01:13:49 +0530

    Updated

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
-# from home.models import Contact
-# from django.contrib import messages
+from home.models import Contact
+from django.contrib import messages
 # from django.contrib.auth.models import User
 # from Blogg.models import Post
 # from django.contrib.auth  import authenticate,  login, logout
@@ -13,19 +13,18 @@ def home(request):
     # return render(request, 'home/home.html')
 
 def contact(request):
-    # if request.method=="POST":
-    #     name=request.POST['name']
-    #     email=request.POST['email']
-    #     phone=request.POST['phone']
-    #     content =request.POST['content']
-    #     if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4 or phone.isnumeric()==False:
-    #         messages.error(request, "Please fill the form correctly")
-    #     else:
-    #         contact=Contact(name=name, email=email, phone=phone, content=content)
-    #         contact.save()
-    #         messages.success(request, "Your message has been successfully sent")
-    # return render(request, "home/contact.html")
-    pass
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content =request.POST['content']
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4 or phone.isnumeric()==False:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact=Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+    return render(request, "home/contact.html")
 
 def about(request):
     # return render(request, 'home/about.html')
