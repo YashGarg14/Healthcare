77d25e8c4c2ae78217050008861714ed1605eec5
YashGarg14 <yash.raj.garg2003@gmail.com>
2022-11-14 23:30:59 +0530

    Updated

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name = 'about'),
     path('contact', views.contact, name = 'contact'),
-    path('signup', views.handleSignUp, name="handleSignUp"),
-    path('login', views.handeLogin, name="handleLogin"),
-    path('logout', views.handelLogout, name="handleLogout"),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('consultation', views.consultation, name='consultation'),
+    path('ambulance', views.ambulance, name='ambulance'),
+    path('AR_tut', views.AR_tut, name='AR_tut'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render, HttpResponse, redirect
 from home.models import Contact
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib import messages
-# from django.contrib.auth.models import User
-# from Blogg.models import Post
-# from django.contrib.auth  import authenticate,  login, logout
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-
-
-def home(request):
-    return render(request, 'home/home.html')
 
 def contact(request):
     if request.method=="POST":
@@ -25,71 +21,61 @@ def contact(request):
             messages.success(request, "Your message has been successfully sent")
     return render(request, "home/contact.html")
 
+def home(request):
+    return render(request, 'home/home.html')
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request,"Your Account has been created successfully")
+        return redirect("signin")
+
+    return render(request, 'home/signup.html')
+
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, 'home/home.html', {'username':username})
+
+        else:
+            messages.error(request, "Bad Credentials")
+            return redirect('home')
+
+    return render(request, 'home/login.html')
+
+def signout(request):
+    logout(request)
+    messages.success(request,"Logout successfully")
+    return redirect("home")
+
 def about(request):
-    # return render(request, 'home/about.html')
     pass
 
-def handleSignUp(request):
-    # if request.method=="POST":
-    #     # Get the post parameters
-    #     username=request.POST['username']
-    #     email=request.POST['email']
-    #     fname=request.POST['fname']
-    #     lname=request.POST['lname']
-    #     pass1=request.POST['pass1']
-    #     pass2=request.POST['pass2']
+def consultation(request):
+    return render(request, 'home/consultation.html')
 
-    #     # check for errorneous input
-    #     if len(username)<10:
-    #         messages.error(request, " Your user name must be under 10 characters")
-    #         return redirect('home')
+def ambulance(request):
+    return render(request, 'home/call_ambulance.html')
 
-    #     if not username.isalnum():
-    #         messages.error(request, " User name should only contain letters and numbers")
-    #         return redirect('home')
-    #     if (pass1!= pass2):
-    #          messages.error(request, " Passwords do not match")
-    #          return redirect('home')
-
-    #     # Create the user
-    #     myuser = User.objects.create_user(username, email, pass1)
-    #     myuser.first_name= fname
-    #     myuser.last_name= lname
-    #     myuser.save()
-    #     messages.success(request, " Your Blogg account has been successfully created")
-    #     return redirect('home')
-
-    # else:
-    #     return HttpResponse("404 - Not found")
-    # return HttpResponse("login")
-    return render(request, "home/signup.html")
-
-
-def handeLogin(request):
-    # if request.method=="POST":
-    #     # Get the post parameters
-    #     loginusername=request.POST['loginusername']
-    #     loginpassword=request.POST['loginpassword']
-
-    #     user=authenticate(username= loginusername, password= loginpassword)
-    #     if user is not None:
-    #         login(request, user)
-    #         messages.success(request, "Successfully Logged In")
-    #         return redirect("home")
-    #     else:
-    #         messages.error(request, "Invalid credentials! Please try again")
-    #         return redirect("home")
-
-    # return HttpResponse("404- Not found")
-    pass
-
-def handelLogout(request):
-    # logout(request)
-    # messages.success(request, "Successfully logged out")
-    # return redirect('home')
-    pass
-
-
-def about(request): 
-    # return render(request, "home/about.html")
-    pass
+def AR_tut(request):
+    return render(request, 'home/AR_tut.html')
