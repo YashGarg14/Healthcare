7f67d82587dd2477ff5f89b68a7ba483c00e0c3b
Yash Garg <yash.raj.garg2003@gmail.com>
2022-11-21 22:51:54 +0530

    updated

--- home/models.py
+++ home/models.py
@@ -23,3 +23,15 @@ class Contact(models.Model):
 
      def __str__(self):
           return "Message from " + self.name + ' - ' + self.email
+
+
+class Doctor(models.Model):
+     sno= models.AutoField(primary_key=True)
+     name= models.CharField(max_length=255)
+     phone= models.CharField(max_length=13)
+     email= models.CharField(max_length=100)
+     Type= models.CharField(max_length=13)
+     timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
+
+     def __str__(self):
+          return "Message from " + self.name + ' - ' + self.email
